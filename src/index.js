a3acabeae841337bad6826acb1719536560aa649
edwardkerry <edwardkerry@gmail.com>
2018-04-14 12:12:18 +0100

    Adds Hot Module reloading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Basic application setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Basic React hot Rendering!';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
