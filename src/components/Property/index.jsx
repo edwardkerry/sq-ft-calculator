82922ebf1b1e229c51d2e59c50925fa8c30eb09b
edwardkerry <edwardkerry@gmail.com>
2018-04-15 17:40:46 +0100

    Add onclick and active to Property

--- src/components/Property/index.jsx
+++ src/components/Property/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { string, number } from 'prop-types';
+import { string, number, bool, func } from 'prop-types';
 
-const Property = ({address, price, floorArea}) => (
-  <li className='property'>
-    <p className='address'>Address: {address}</p>
-    <p className='price'>Value: £{price}</p>
-    <p className='floor_area'>Floor area(sq ft): {floorArea}</p>
+const getClassName = (selected) => selected ? ' active' : ''
+
+const Property = ({address, price, floorArea, select, selected}) => (
+  <li>
+    <a className={`property${getClassName(selected)}`}
+      onClick={select}
+    >
+      <p className='address'>Address: {address}</p>
+      <p className='price'>Value: £{price}</p>
+      <p className='floor_area'>Floor area(sq ft): {floorArea}</p>
+    </a>
   </li>
 );
 
 Property.propTypes = {
   address: string.isRequired,
   price: number. isRequired,
-  floorArea: number.isRequired
+  floorArea: number.isRequired,
+  selected: bool.isRequired,
+  select: func.isRequired
 };
 
 export default Property;
--- src/components/Property/index.spec.js
+++ src/components/Property/index.spec.js
@@ -7,29 +7,74 @@ describe('Property', () => {
   const testPrice = 100000
   const testArea = 1000
 
-  const wrapper = shallow(<Property address={testAddress} price={testPrice} floorArea={testArea} />)
 
-  it('renders', () => {
-    const component = renderer.create(
-      <Property
-        address={testAddress}
-        price={testPrice}
-        floorArea={testArea}
+  describe('when not selected', () => {
+    it('renders ', () => {
+      const component = renderer.create(
+        <Property
+          address={testAddress}
+          price={testPrice}
+          floorArea={testArea}
+          selected={false}
+          select={() => { }}
+
+      />)
+      const tree = component.toJSON()
+
+      expect(tree).toMatchSnapshot()
+    });
+
+    const wrapper = shallow(<Property
+      address={testAddress}
+      price={testPrice}
+      floorArea={testArea}
+      selected={false}
+      select={() => { }}
     />)
-    const tree = component.toJSON()
 
-    expect(tree).toMatchSnapshot()
+    it('displays the given address', () => {
+      expect(wrapper.find('.address').text()).toEqual(`Address: ${testAddress}`)
+    });
+
+    it('displays the given price', () => {
+      expect(wrapper.find('.price').text()).toEqual(`Value: £${testPrice}`)
+    });
+
+    it('displays the given floor area', () => {
+      expect(wrapper.find('.floor_area').text()).toEqual(`Floor area(sq ft): ${testArea}`)
+    });
+
+    it('does not have an active className', () => {
+      expect(wrapper.find('a').hasClass('active')).toEqual(false)
+    })
   });
 
-  it('displays the given address', () => {
-    expect(wrapper.find('.address').text()).toEqual(`Address: ${testAddress}`)
-  });
+  describe('when selected', () => {
+    it('renders ', () => {
+      const component = renderer.create(
+        <Property
+          address={testAddress}
+          price={testPrice}
+          floorArea={testArea}
+          selected={true}
+          select={() => { }}
 
-  it('displays the given price', () => {
-    expect(wrapper.find('.price').text()).toEqual(`Value: £${testPrice}`)
-  });
+        />)
+      const tree = component.toJSON()
 
-  it('displays the given floor area', () => {
-    expect(wrapper.find('.floor_area').text()).toEqual(`Floor area(sq ft): ${testArea}`)
+      expect(tree).toMatchSnapshot()
+    });
+
+    const wrapper = shallow(<Property
+      address={testAddress}
+      price={testPrice}
+      floorArea={testArea}
+      selected={true}
+      select={() => { }}
+    />)
+
+    it('has an active className', () => {
+      expect(wrapper.find('a').hasClass('active')).toEqual(true)
+    });
   });
 });
